a7ee213c2d751ddaf26b088bacd107f9c46ebb6a
xzw <1309240100@qq.com>
2019-09-17 18:36:32 +0800

    数据预处理

--- pre_data.py
+++ pre_data.py
@@ -16,15 +16,15 @@ class WordToken(object):
         """
         words_count = {}
         for file in file_list:
-            with open(file, 'r') as file_object:
+            with open(file, 'r', encoding='utf-8') as file_object:
                 for line in file_object.readlines():
                     line = line.strip()
                     seg_list = jieba.cut(line)
-                    for str in seg_list:
-                        if str in words_count:
-                            words_count[str] = words_count[str] + 1
+                    for strr in seg_list:
+                        if strr in words_count:
+                            words_count[strr] = words_count[strr] + 1
                         else:
-                            words_count[str] += 1
+                            words_count[strr] = 1
         sorted_list = [[v[1], v[0]] for v in words_count.items()]
         sorted_list.sort(reverse=True)
         for index, item in enumerate(sorted_list):
@@ -44,9 +44,9 @@ class WordToken(object):
         else:
             return None
 
-    def id2word(self,id):
+    def id2word(self, id):
         id = int(id)
-        if id in self.id2word_dict[id]:
+        if id in self.id2word_dict:
             return self.id2word_dict[id]
         else:
             return None
